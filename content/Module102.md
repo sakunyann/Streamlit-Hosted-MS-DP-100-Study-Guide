fad352a03a79dc99f2935b7b460e3779260e5458
Sakura Koffron <147252329+sakunyann@users.noreply.github.com>
2024-06-02 10:34:11 +0000

    update

--- content/Module102.md
+++
@@ -1,8 +0,0 @@
-# Design a machine learning solution
-
-## Design a machine learning model training solution
-32 min - Module - 7 Units
-
-<br> 
-
-## Unit 1/7 - Introduction
---
+++ pages/1_1.1_Design_a_data_ingestion_strategy_for_machine_learning_projects.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(page_title="DP 100 - 1.1",
+                   page_icon=":memo:",
+                   layout="wide")
+
+
+st.markdown("""
+# Design a machine learning solution
+
+## Design a data ingestion strategy for machine learning projects
+33 min - Module - 7 Units
+
+<br> 
+
+## Unit 1/7 - Introduction
+
+"""
+)
